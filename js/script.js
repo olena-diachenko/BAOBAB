1f158e5cd5c1b3ef3c27b952c14d2e2695f54573
Olena Diachenko <ellenka24@gmail.com>
2023-02-07 22:12:17 +0200

    tablet breakpoints added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,22 @@
 $('.clients__wrap').slick({
-  // centerMode: true,
   arrows: false,
   centerPadding: '0',
   slidesToShow: 6,
   responsive: [
     {
-      breakpoint: 768,
+      breakpoint: 1279,
       settings: {
         dots: true,
         arrows: false,
-        // centerMode: true,
         centerPadding: '15px',
         slidesToShow: 3
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 767,
       settings: {
         dots: true,
         arrows: false,
-        // centerMode: true,
         centerPadding: '15px',
         slidesToShow: 1
       }
