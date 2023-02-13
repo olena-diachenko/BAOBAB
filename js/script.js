20f3d30907f6b40c4aa9c7ae57c2aad87bed11ff
Olena Diachenko <ellenka24@gmail.com>
2023-02-13 14:11:48 +0200

    video button added, scroll behavior for Safari added, type of search input changed

--- js/script.js
+++ js/script.js
@@ -14,6 +14,53 @@ $(document).ready(function () {
   });
 })
 
+
+// Video
+
+jQuery(function($) {
+  //play video btn
+  $(".position-relative video").removeAttr("controls"); //hide controls by default
+  //on clicking play button
+  $(".video-section").each(function() {
+    $(".play-btn").click(function() {
+      var video = $(this)
+        .closest(".position-relative")
+        .find("video")
+        .get(0);
+      video.play();
+      video.controls = true;
+      $(this).css("visibility", "hidden");
+      return false;
+    });
+
+    //when video is paused
+    $(this)
+      .find("video")
+      .click(function() {
+        var video = $(this).get(0);
+        video.pause();
+        video.controls = false;
+        $(this)
+          .siblings(".play-btn")
+          .css("visibility", "visible");
+        return false;
+      });
+
+    //when video has ended
+    $(this)
+      .find("video")
+      .on("ended", function() {
+        $(this).get(0).controls = false;
+        $(this)
+          .siblings(".play-btn")
+          .css("visibility", "visible");
+        $(this)
+          .get(0)
+          .load();
+      });
+  });
+});
+ 
 // Slider
 
 $('.clients__wrap').slick({
@@ -40,4 +87,19 @@ $('.clients__wrap').slick({
       }
     }
   ]
+});
+
+// Scroll-behavior for Safari
+
+$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (t) {
+    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+        var e = $(this.hash);
+        e = e.length ? e : $("[name=" + this.hash.slice(1) + "]"), e.length && (t.preventDefault(), $("html, body").animate({
+            scrollTop: e.offset().top
+        }, 400, function () {
+            var t = $(e);
+            if (t.focus(), t.is(":focus")) return !1;
+            t.attr("tabindex", "-1"), t.focus()
+        }))
+    }
 });
